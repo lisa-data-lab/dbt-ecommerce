fd77c24db669cd3f90766a345105d3a4ff3d8884
Lisa Zeng <lisazeng1635@gmail.com>
2026-07-15 21:12:12 +0000

    new models and intermediate models

--- models/intermediate/Olist/int_order_items_summary_olist.sql
+++ models/intermediate/Olist/int_order_items_summary_olist.sql
@@ -8,10 +8,10 @@ aggregated as (
 
     select
         order_id,
-        count(order_item_id) as total_items,
-        sum(price) as total_item_price,
-        sum(freight_value) as total_freight_value,
-        sum(price) + sum(freight_value) as total_order_cost
+        count(*) as total_items,
+        sum(cast(price as decimal(10, 2))) as total_item_price,
+        sum(cast(freight_value as decimal(10, 2))) as total_freight_value,
+        sum(cast(price as decimal(10, 2))) + sum(cast(freight_value as decimal(10, 2))) as total_order_cost
 
     from order_items
     group by 1
